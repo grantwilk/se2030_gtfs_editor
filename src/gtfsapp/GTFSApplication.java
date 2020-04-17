3463274fb57de0446e9f1b9fb3ae79159c31d5c3
Grant Wilk <grant@remington.pro>
2020-04-17 12:52:20 -0500

    implemented GTFSApplication and start() method

--- src/gtfsapp/GTFSApplication.java
+++ src/gtfsapp/GTFSApplication.java
@@ -3,7 +3,12 @@ package gtfsapp;
 import gtfsapp.gui.GTFSMainController;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 /**
  * @author Grant Wilk
@@ -12,14 +17,41 @@ import javafx.stage.Stage;
  */
 public class GTFSApplication extends Application {
 
-	private GTFSMainController mainController;
+	/**
+	 * The maximum height of the main stage in pixels
+	 */
+	private static final double MAIN_STAGE_MINIMUM_WIDTH = 500;
 
 	/**
-	 * 
-	 * @param mainStage
+	 * The minimum height of the main stage in pixels
 	 */
-	public void start(Stage mainStage){
+	private static final double MAIN_STAGE_MINIMUM_HEIGHT = 500;
 
-	}
+    /**
+	 * The entry point for the application
+     * @param mainStage - the stage that will be running the main GUI
+     */
+    public void start(Stage mainStage) throws IOException {
+
+        // load the FXML file
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/fxml/mainGUI.fxml"));
+        Parent root = loader.load();
+
+        // configure basic stage attributes
+		mainStage.setTitle("GTFS Editor");
+
+		// create and set stage scene
+		Scene mainScene = new Scene(root);
+		mainStage.setScene(mainScene);
+
+		// get and configure main controller
+		GTFSMainController mainController = loader.getController();
+		mainController.setStage(mainStage);
+		mainController.setScene(mainScene);
+
+		// show the stage
+		mainStage.show();
+
+    }
 
 }
