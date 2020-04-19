c59b1e67f9a97a80d01ef3221edfd70f40ce9f18
Grant Wilk <grant@remington.pro>
2020-04-18 21:57:29 -0500

    added method for randomly generating IDs

--- src/gtfsapp/id/StopTimeID.java
+++ src/gtfsapp/id/StopTimeID.java
@@ -7,10 +7,36 @@ package gtfsapp.id;
  */
 public class StopTimeID extends GTFSID {
 
-    /** Constructor for StopTimeID
+    /**
+     * Constructor for StopTimeID
      * @param id String id to be associated with this object
      */
     public StopTimeID(String id) {
         super(id);
+    }
+
+    /**
+     * Generates a random stop time ID
+     */
+    public StopTimeID() {
+        super(generateID("ST"));
+    }
+
+    /**
+     * Procedurally generates a unique ID string
+     * @param prefix - a prefix for the ID
+     * @return a unique ID string
+     */
+    public static String generateID(String prefix) {
+
+        int count = 0;
+        StopTimeID id;
+
+        // increment count and test every ID until we are unique
+        do {
+            id = new StopTimeID(prefix + count++);
+        } while (exists(id));
+
+        return id.toString();
     }
 }
