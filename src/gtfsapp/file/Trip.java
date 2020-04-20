73306c6d86ad275e019d8ab932ab85d12f0840eb
Grant Wilk <grant@remington.pro>
2020-04-20 11:39:14 -0500

    performed code review, tweaked some documentation, and reorganized methods

--- src/gtfsapp/file/Trip.java
+++ src/gtfsapp/file/Trip.java
@@ -1,13 +1,9 @@
 package gtfsapp.file;
 
-import gtfsapp.id.RouteID;
-import gtfsapp.id.StopID;
-import gtfsapp.id.StopTimeID;
+import gtfsapp.id.*;
 
-import gtfsapp.id.TripID;
 import javafx.geometry.Point2D;
 
-import java.lang.reflect.Array;
 import java.util.*;
 
 /**
@@ -17,14 +13,30 @@ import java.util.*;
  */
 public class Trip extends GTFSElement {
 
-    private Feed feed;
-    private HashMap<StopID, Stop> stops;
-    private HashMap<StopTimeID, StopTime> stopTimes;
+    /**
+     * The feed the trip belongs to
+     */
+    private final Feed feed;
+
+    /**
+     * A map of all stop IDs and stops contained within this trip
+     */
+    private HashMap<StopID, Stop> stops = new HashMap<>();
+
+    /**
+     * A map of all stop time IDs and stop times contained within this trip
+     */
+    private HashMap<StopTimeID, StopTime> stopTimes = new HashMap<>();
+
+    /**
+     * The head sign for this trip
+     */
     private String headSign;
 
     /**
      * Constructor for the trip object with an id and feed as parameters
-     * @param id for the trip
+     *
+     * @param id   for the trip
      * @param feed for the trip
      */
     public Trip(Feed feed, String id) {
@@ -33,227 +45,40 @@ public class Trip extends GTFSElement {
     }
 
     /**
-     * ???Not sure If I did this right, I really struggled with maps in Data structures
-     * Puts all of the stopTimes passed in, into the hash map stopTimes
-     * @param stopTimes to be added to the stopTimes Hashmaps
+     * Clears all of the stop times from the internal hash map
      */
-    public void addAllStopTimes(ArrayList<StopTime> stopTimes) {
-
-        // TODO - Mason, a really easy way to do this is to use the HashMap's built in .addAll() method!
-        // TODO - With that, you shouldn't have to use more than one line of code. -Grant
-
-        for(int i = 0; i < stopTimes.size(); i++){
-            this.stopTimes.putAll((Map<? extends StopTimeID, ? extends StopTime>) stopTimes);
-
-        }
-
+    public void clearStopTimes() {
+        stopTimes.clear();
+        stops.clear();
     }
 
     /**
      * @param stopTime
      */
     public void addStopTime(StopTime stopTime) {
-
+        // TODO - this needs to be implemented!
     }
 
     /**
-     * Clears all of the stop times from the hash map
-     * @reutrn void
-     */
-    public void clearStopTimes() {
-        stopTimes.clear();
-    }
-
-    /**
-     * Searches the hash map of stops, by using a stop ID, to see if that stop
-     * is on the trip
-     * @param id of the stop
-     */
-    public boolean containsStop(StopID id) {
-        return stops.containsValue(id);
-    }
-
-    /**
-     * Searches the hash map of stop times, by using a stopTimeID to see if that stop time
-     * is on the trip
-     * @param id of the stop time
-     */
-    public boolean containsStopTime(StopTimeID id) {
-        return stopTimes.containsKey(id);
-    }
-
-    /**
-     * Not yet implemented
-     * @return
-     */
-    public Stop getActiveStop() {
-        throw new UnsupportedOperationException();
-    }
-
-    /**
-     * Not yet implemented
-     * @return
-     */
-    public StopTime getActiveStopTime() {
-        throw new UnsupportedOperationException();
-    }
-
-    /**
-     * Unsupported
-     * @return
-     */
-    public double getAvgSpeed() {
-        throw new UnsupportedOperationException();
-        //return 0;
-    }
-
-    /**
-     * Unsupported
-     * @return
-     */
-    public Point2D getBusPosition() {
-        throw new UnsupportedOperationException();
-        //return null;
-    }
-
-    /**
+     * Adds a list of the stop times to the internal map of stop times
      *
-     * @return
+     * @param stopTimes - the list of stop times
      */
-    public ArrayList<RouteID> getContainingRouteIDs() {
-        return null;
-    }
+    public void addAllStopTimes(ArrayList<StopTime> stopTimes) {
 
-    /**
-     *
-     * @return
-     */
-    public ArrayList<Route> getContainingRoutes() {
-        return null;
-    }
+        // for each stop time in the array
+        for (StopTime stopTime : stopTimes) {
+            // add the list of stop times to the stop times map
+            this.stopTimes.put((StopTimeID) stopTime.getID(), stopTime);
+            // add the stop time's stop to the stops map
+            stops.put((StopID) stopTime.getStop().getID(), stopTime.getStop());
+        }
 
-    /**
-     * Not yet implemented
-     * @return disntace between stops
-     */
-    public double getDistance() {
-        throw new UnsupportedOperationException();
-    }
-
-    /**
-     * Not yet implemented
-     * @return time between the stops
-     */
-    public double getDuration() {
-        throw new UnsupportedOperationException();
-    }
-
-    /**
-     * Getter for the feed for the trip
-     * @return the feed the trip is in
-     */
-    public Feed getFeed() {
-        return this.feed;
-    }
-
-    /**
-     * Not yet implemented
-     * @return
-     */
-    public Stop getNextStop() {
-        throw new UnsupportedOperationException();
-    }
-
-    /**
-     * Not yet implemeted
-     * @return
-     */
-    public StopTime getNextStopTime() {
-        throw new UnsupportedOperationException();
-    }
-
-    /**
-     *
-     * @return
-     */
-    public Stop getPreviousStop() {
-        throw new UnsupportedOperationException();
-    }
-
-    /**
-     *
-     * @return
-     */
-    public StopTime getPreviousStopTime() {
-        throw new UnsupportedOperationException();
-    }
-
-    /**
-     * Returns the stop for the given stopID on the trip
-     * @param stop StopID for the wanted stop
-     */
-    public Stop getStopByID(StopID stop) {
-        return feed.getStopByID(stop);
-    }
-
-    /**
-     *
-     * @return
-     */
-    public ArrayList<StopID> getStopIDs() {
-        throw new UnsupportedOperationException();
-    }
-
-    /**
-     *
-     * @return
-     */
-    public ArrayList<Stop> getStops() {
-        return null;
-    }
-
-    /**
-     * Gets the stop time of the requested stop from the ID
-     * @param id of the stop time wanted
-     */
-    public StopTime getStopTimeByID(StopTimeID id) {
-        return stopTimes.get(id);
-    }
-
-    /**
-     * Returns the StopTimeIDS for this trip
-     * @return an array list of the stopTimeIDs
-     */
-    public ArrayList<StopTimeID> getStopTimeIDs() {
-
-        Set<StopTimeID> stopTimeIDSet = stopTimes.keySet();
-        ArrayList<StopTimeID> stopTimeIDS = null;
-        stopTimeIDS.addAll(stopTimeIDSet);
-
-        return stopTimeIDS;
-    }
-
-    /**
-     * Gets an ArrayList of StopTimes for the trip
-     * @return the ArrayList of stopTimes
-     */
-    public ArrayList<StopTime> getStopTimes() {
-        Collection<StopTime> stopTimeSet =stopTimes.values();
-        ArrayList<StopTime> stopTimeArrayList = null;
-        stopTimeArrayList.addAll(stopTimeSet);
-        return stopTimeArrayList;
-    }
-
-    /**
-     * Not yet implemented
-     * @return
-     */
-    public boolean isActive() {
-        throw new UnsupportedOperationException();
     }
 
     /**
      * Removes the stop time from the stopTimes hash map
+     *
      * @param stopTime to be removed
      * @return the stopTime removed
      */
@@ -263,6 +88,7 @@ public class Trip extends GTFSElement {
 
     /**
      * Removes the Stop time that was previously at the id passed
+     *
      * @param id of the stop time to be removed
      * @return the StopTimeID removed
      */
@@ -271,23 +97,195 @@ public class Trip extends GTFSElement {
     }
 
     /**
-     * Setter for the headSign for the trip
-     * @param headSign the headSign to be assigned
+     * Checks to see if this trip contains a specific stop
+     *
+     * @param id - the ID of the stop
      */
-    public void setHeadSign(String headSign) {
-        this.headSign = headSign;
+    public boolean containsStop(StopID id) {
+        return stops.containsKey(id);
     }
 
     /**
-     * Getter for the headSign of the trip
-     * @return the headSign
+     * Checks to see if this trip contains a specific stop time
+     *
+     * @param id - the ID of the stop time
+     */
+    public boolean containsStopTime(StopTimeID id) {
+        return stopTimes.containsKey(id);
+    }
+
+    /**
+     * @return
+     */
+    public ArrayList<Route> getContainingRoutes() {
+        // TODO - needs implementation eventually
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * @return
+     */
+    public ArrayList<RouteID> getContainingRouteIDs() {
+        // TODO - needs implementation eventually
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Gets a stop time contained within the trip by its ID
+     *
+     * @param id - the ID of the stop time
+     */
+    public StopTime getStopTimeByID(StopTimeID id) {
+        return stopTimes.get(id);
+    }
+
+    /**
+     * Gets all of the stop time IDs contained within the trip
+     *
+     * @return a list of stop time IDs contained within the trip
+     */
+    public ArrayList<StopTimeID> getStopTimeIDs() {
+        Set<StopTimeID> stopTimeIDSet = stopTimes.keySet();
+        return new ArrayList<>(stopTimeIDSet);
+    }
+
+    /**
+     * Gets all of the stop times contained within the trip
+     *
+     * @return a list of stop times contained within the trip
+     */
+    public ArrayList<StopTime> getStopTimes() {
+        Collection<StopTime> stopTimeSet = stopTimes.values();
+        return new ArrayList<>(stopTimeSet);
+    }
+
+    /**
+     * Gets a stop contained within the trip by its ID
+     *
+     * @param id - the ID of the stop
+     */
+    public Stop getStopByID(StopID id) {
+        return stops.get(id);
+    }
+
+    /**
+     * @return
+     */
+    public ArrayList<StopID> getStopIDs() {
+        // TODO - This needs to be implemented!
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    public ArrayList<Stop> getStops() {
+        // TODO - This needs to be implemented!
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    public StopTime getNextStopTime() {
+        // TODO - needs implementation eventually
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * @return
+     */
+    public StopTime getPreviousStopTime() {
+        // TODO - needs implementation eventually
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * @return
+     */
+    public Stop getNextStop() {
+        // TODO - needs implementation eventually
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * @return
+     */
+    public Stop getPreviousStop() {
+        // TODO - needs implementation eventually
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * @return
+     */
+    public double getAvgSpeed() {
+        // TODO - needs implementation eventually
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * @return
+     */
+    public Point2D getBusPosition() {
+        // TODO - needs implementation eventually
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * @return
+     */
+    public double getDistance() {
+        // TODO - needs implementation eventually
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * @return
+     */
+    public double getDuration() {
+        // TODO - needs implementation eventually
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Getter for the head sign of the trip
+     *
+     * @return the head sign of the trip
      */
     public String getHeadSign() {
         return headSign;
     }
 
     /**
+     * Setter for the head sign for the trip
+     *
+     * @param headSign - the headSign of the trip
+     */
+    public void setHeadSign(String headSign) {
+        this.headSign = headSign;
+    }
+
+    /**
+     * @return
+     */
+    public boolean isActive() {
+        // TODO - needs implementation eventually
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Getter for the feed that contains this trip
+     *
+     * @return the feed that contains this trip
+     */
+    public Feed getFeed() {
+        return feed;
+    }
+
+    /**
      * Gets the trip's title to be displayed in the GUI
+     *
      * @return the trip's title
      */
     @Override
@@ -297,6 +295,7 @@ public class Trip extends GTFSElement {
 
     /**
      * Gets the trip's subtitle to be displayed in the GUI
+     *
      * @return the trip's subtitle
      */
     @Override
@@ -307,6 +306,7 @@ public class Trip extends GTFSElement {
 
     /**
      * Gets the trip's attributes to be displayed in the GUI
+     *
      * @return a HashMap<Attribute Title, Attribute Value> of the trip's attributes
      */
     @Override
