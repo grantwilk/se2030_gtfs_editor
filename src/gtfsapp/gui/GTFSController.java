7b87397acc4eb8cabe2628623f9bc15f307d312a
Grant Wilk <grant@remington.pro>
2020-04-19 22:36:38 -0500

    reformatted code and added additional stage control methods

--- src/gtfsapp/gui/GTFSController.java
+++ src/gtfsapp/gui/GTFSController.java
@@ -1,5 +1,6 @@
 package gtfsapp.gui;
 
+import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -14,7 +15,22 @@ public abstract class GTFSController {
     private Stage stage;
 
     /**
-     * Gets the controller's scene and returns it
+     * Closes the window
+     */
+    public void close() {
+        stage.close();
+    }
+
+    /**
+     * Quits the application
+     */
+    public void quit() {
+        Platform.exit();
+    }
+
+    /**
+     * Gets the controller's scene
+     *
      * @return the controller's scene
      */
     public Scene getScene() {
@@ -22,7 +38,8 @@ public abstract class GTFSController {
     }
 
     /**
-     * Gets the controller's stage and returns it
+     * Gets the controller's stage
+     *
      * @return the controller's stage
      */
     public Stage getStage() {
@@ -30,7 +47,17 @@ public abstract class GTFSController {
     }
 
     /**
+     * Gets the title of the window
+     *
+     * @return the title of the window
+     */
+    public String getWindowTitle() {
+        return stage.getTitle();
+    }
+
+    /**
      * Sets the controller's scene
+     *
      * @param scene - the scene to set
      */
     public void setScene(Scene scene) {
@@ -39,6 +66,7 @@ public abstract class GTFSController {
 
     /**
      * Sets the controllers stage
+     *
      * @param stage - the stage to set
      */
     public void setStage(Stage stage) {
@@ -46,19 +74,30 @@ public abstract class GTFSController {
     }
 
     /**
+     * Sets the window's always on top property
+     *
+     * @param alwaysOnTop - true if the window should be always on top, false otherwise
+     */
+    public void setWindowAlwaysOnTop(boolean alwaysOnTop) {
+        stage.setAlwaysOnTop(alwaysOnTop);
+    }
+
+    /**
+     * Sets the window's resizable property
+     *
+     * @param isResizable - true if the window should be resizable, false otherwise
+     */
+    public void setWindowResizable(boolean isResizable) {
+        stage.setResizable(isResizable);
+    }
+
+    /**
      * Sets the title of the window
+     *
      * @param windowTitle - the title of the window
      */
     public void setWindowTitle(String windowTitle) {
         stage.setTitle(windowTitle);
-    }
-
-    /**
-     * Gets the title of the window
-     * @return the title of the window
-     */
-    public String getWindowTitle() {
-        return stage.getTitle();
     }
 
 }
