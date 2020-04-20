47a7dd105b4039b0b5720c702ca927a8942527df
Grant Wilk <grant@remington.pro>
2020-04-20 10:48:47 -0500

    implemented gtfs dialog controller

--- src/gtfsapp/gui/dialog/GTFSDialogController.java
+++ src/gtfsapp/gui/dialog/GTFSDialogController.java
@@ -1,7 +1,7 @@
 package gtfsapp.gui.dialog;
 
 import gtfsapp.gui.GTFSController;
-import javafx.stage.Stage;
+import gtfsapp.gui.main.GTFSMainController;
 
 /**
  * @author Grant Wilk
@@ -9,5 +9,26 @@ import javafx.stage.Stage;
  * @created 15-Apr-2020 1:20:18 PM
  */
 public abstract class GTFSDialogController extends GTFSController {
+
+    /**
+     * The main controller that the dialog was invoked from
+     */
+    public GTFSMainController mainController;
+
+    /**
+     * Gets the dialog's main controller
+     * @return the dialog's main controller
+     */
+    public GTFSMainController getMainController() {
+        return mainController;
+    }
+
+    /**
+     * Sets the dialog's main controller
+     * @param mainController - the dialog's main controller
+     */
+    public void setMainController(GTFSMainController mainController) {
+        this.mainController = mainController;
+    }
 
 }
