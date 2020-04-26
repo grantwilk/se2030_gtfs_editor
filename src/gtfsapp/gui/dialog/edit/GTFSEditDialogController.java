c1316ec2cb66ef73fce4aece5a7487dc6fbf832d
Grant Wilk <grant@remington.pro>
2020-04-26 12:29:53 -0500

    implemented edit dialog controller

--- src/gtfsapp/gui/dialog/edit/GTFSEditDialogController.java
+++ src/gtfsapp/gui/dialog/edit/GTFSEditDialogController.java
@@ -8,11 +8,32 @@ import gtfsapp.gui.dialog.GTFSDialogController;
  * @version 1.0
  * @created 15-Apr-2020 1:20:18 PM
  */
-public class GTFSEditDialogController extends GTFSDialogController {
+public abstract class GTFSEditDialogController extends GTFSDialogController {
 
     /**
-     * The currently selected element
+     * The edit dialog's element
      */
-    private GTFSElement selectedElement;
+    private GTFSElement element;
+
+    /**
+     * Sets the edit dialog's element
+     * @param element - the edit dialog's element
+     */
+    public void setElement(GTFSElement element) {
+        this.element = element;
+    }
+
+    /**
+     * Gets the edit dialog's element
+     * @return the edit dialog's element
+     */
+    public GTFSElement getElement() {
+        return element;
+    }
+
+    /**
+     * Updates the attributes associated with the edit dialog's elements
+     */
+    public abstract void updateAttributes();
 
 }
