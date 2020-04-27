ce5ce0f1bc487d589c2d9a5c3058a93c4cd4924c
primeaum <primeaum@msoe.edu>
2020-04-26 22:44:04 -0500

    Created test folder and new test file

---
+++ test/test/GTFSFileTest.java
@@ -0,0 +1,30 @@
+package test;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GTFSFileTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void load() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void getFeed() {
+    }
+}
