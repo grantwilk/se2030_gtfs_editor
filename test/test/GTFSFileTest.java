f95c70786cb74acdc7d175d7cdd835ceacea8bc4
primeaum <primeaum@msoe.edu>
2020-04-27 13:32:23 -0500

    Bagan writing file validation tests

--- test/test/GTFSFileTest.java
+++ test/test/GTFSFileTest.java
@@ -1,8 +1,16 @@
 package test;
 
+import gtfsapp.file.GTFSFile;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -25,6 +33,17 @@ class GTFSFileTest {
 
     @Test
     void validateStops() {
+        File file = new File("C:/Users/primeaum/Documents/SE2030Project/2020-project-team-delta/samples/rainy-day-1/stops.txt");
+        List<File> files = new ArrayList<>();
+        files.add(file);
+        GTFSFile testFile;
+        try {
+            testFile = new GTFSFile(files);
+            List<String> lines = Files.readAllLines(file.toPath());
+            testFile.validateStops(lines);
+            
+        } catch(IOException e) { }
+
 
     }
 }
