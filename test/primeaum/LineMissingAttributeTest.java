2869722fca99047ddd2f6f057f9a425001a707ab
primeaum <primeaum@msoe.edu>
2020-04-27 19:52:27 -0500

    Began implementing stop validation tests

---
+++ test/primeaum/LineMissingAttributeTest.java
@@ -0,0 +1,18 @@
+package primeaum;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class LineMissingAttributeTest extends ValidateStopsTest {
+
+
+    @Test
+    void validateLineMissingTripID() {
+
+
+    }
+
+}
